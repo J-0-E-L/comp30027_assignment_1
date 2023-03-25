05531efd900eda20ff464b961df10878c6360e41
tmblack01 <106416111+tmblack01@users.noreply.github.com>
2023-03-25 12:48:05 +1100

    Fixed error in prior probability function

--- train_data.py
+++ train_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 """ Calculate the prior probability of all the class labels for the naive bayes
     model. The argument of the function is the train dataframe and the output
     is a dictionary containing the prior probabilities of each label """
-def calculate_prior_prob(train_df):
+def calculate_prior_prob(train_df : pd.DataFrame):
     prior_prob = {}
     label_instances = train_df["label"]
     labels = np.unique(label_instances)
@@ -12,7 +12,7 @@ def calculate_prior_prob(train_df):
     
     # iterate over all the labels and calculate the prior probabilities
     for label in labels:
-        label_count = (label_instances == label).count()
+        label_count = (label_instances == label).sum()
         prior_prob[label] = label_count / total_instances
 
     return prior_prob
